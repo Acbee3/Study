46245814b3bb78a121439142673b205fec5c6c05
Acbee3 <2463254281@qq.com>
2020-08-10 01:28:43 +0800

    tsc 新增配置

---
+++ 基本工具/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: liuchao
+ * @Date: 2020-08-10 01:18:19
+ * @LastEditTime: 2020-08-10 01:19:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Code/基本工具/index.js
+ */
+
+// 淘宝镜像配置
+// $ npm get registry #查看原本镜像
+// $ npm config set registry http://registry.npm.taobao.org/ #修改成淘宝镜像
+// $ npm config set registry https://registry.npmjs.org/ #镜像还原
