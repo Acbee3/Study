f3a4b9fea3a39c8d1c032c80d3de4976f7e30eed
Acbee3 <2463254281@qq.com>
2021-10-18 00:35:58 +0800

    update

--- C/007.c
+++ C/007.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-09-27 22:52:34
- * @LastEditTime: 2021-10-07 17:13:33
+ * @LastEditTime: 2021-10-18 00:35:41
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /C/007.c
@@ -16,8 +16,9 @@ int main()
     system("pause");
     printf("%f\n", f);
     scanf("%d", &i); // 会阻塞
+    fflush(stdin); //  清空缓存区
     printf("i=%d\n", i);
-    scanf("%c", &c); // 不会阻塞
+    scanf("%c", &c); // 不会阻塞 缓存区
     printf("c=%c\n", c);
     return 0;
 }
---
+++ C/012.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-10 16:38:00
+ * @LastEditTime: 2021-10-10 16:39:17
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /C/012.c
+ */
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    scanf("%d", &c);
+    printf("c=%c\n", c);
+    return 0;
+}
